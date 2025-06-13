ef0929bd083d911e9326e49410e6cc7d1e2fe6f1
Frank B <bamwesigyefrank@gmail.com>
2025-06-14 02:52:23 +0300

    limit to 5

--- emis/eims/views.py
+++ emis/eims/views.py
@@ -458,7 +458,7 @@ def generate_album(request):
             registration_category__iexact=reg_category_form, # Use form value for filtering
             assessment_date__year=assessment_year,
             assessment_date__month=assessment_month
-        ).order_by('full_name').limit(50) # Ensure consistent ordering
+        ).order_by('full_name').limit(5)# Ensure consistent ordering
         print("Number of generated candidates = ", candidate_qs.count())
 
         # Optional level filtering (if applicable for the registration category)
